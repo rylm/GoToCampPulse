f2862427e0e3c70edfe71e4a1da3efd6a227f8f8
Alex Danilov <alexajax@l33th4cker.ru>
2016-07-22 10:38:12 +0300

    Add better legend

--- GoToCampPulse.py
+++ GoToCampPulse.py
@@ -11,42 +11,45 @@ online_counter = 0
 i = 0
 
 client = pymongo.MongoClient()
-db = client.test
+db = client.main
 
-datetime_now = datetime.datetime.fromtimestamp(time.time()).strftime('%c')
-print(datetime_now)
+while True:
+    for second in range(1, 61):
+        print(second)
+    print("================================================\n")
+    datetime_now = datetime.datetime.fromtimestamp(time.time()).strftime('%c')
+    print(datetime_now)
+    print("\n================================================\n")
 
+    print(str(len(users)) + " people:\n")
 
-print(str(len(users)) + " people:")
+    for user_id in users:
+        url += user_id + ","
 
-for user_id in users:
-    url += user_id + ","
+    profiles = requests.get(url).json()['response']
 
-profiles = requests.get(url).json()['response']
-# print(profiles)
+    for user in profiles:
+        first_name = user['first_name']
+        last_name = user['last_name']
+        is_online = user['online']
+        uid = user['uid']
 
-for user in profiles:
-    first_name = user['first_name']
-    last_name = user['last_name']
-    is_online = user['online']
-    uid = user['uid']
+        if is_online:
+            online_counter += 1
 
-    if is_online:
-        online_counter += 1
+        online_message = "is not online" if is_online == 0 else "is online"
 
-    online_message = "is not online" if is_online == 0 else "is online"
+        if 'online_mobile' in user.keys():
+            print(first_name + " " + last_name + " " + online_message, end="")
+            print(" from mobile site or app")
+        else:
+            print(first_name + " " + last_name + " " + online_message)
 
-    if 'online_mobile' in user.keys():
-        print(first_name + " " + last_name + " " + online_message, end="")
-        print(" from mobile site or app")
-    else:
-        print(first_name + " " + last_name + " " + online_message)
+    with open("last_number.txt", "w") as last_record:
+        last_record.write(str(online_counter))
 
-with open("last_number.txt", "w") as last_record:
-    last_record.write(str(online_counter))
-
-db.user_stats.insert_one(
-    {
-        str(int(time.time())): profiles
-    }
-)
+    db.user_stats.insert_one(
+        {
+            str(int(time.time())): profiles
+        }
+    )
