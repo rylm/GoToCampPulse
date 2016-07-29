7bf18b3e68cbe7f6909f838b41ed43dcb891b25f
rylm <alexajax@l33th4cker.ru>
2016-07-29 16:24:17 +0300

    Final commit in GoTo Camp

--- csvparse.py
+++ csvparse.py
@@ -16,10 +16,10 @@ for record in records:
     for user in record[time]:
         if user['online'] == 1:
             online_counter += 1
-    online_records.append([time, online_counter])
+    online_records.append([time, online_counter, datetime.datetime.fromtimestamp(int(time))])
 
 with open('data.csv', 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    csvwriter.writerow(['time', 'people_online'])
+    csvwriter.writerow(['time', 'people_online', 'datetime'])
     for online_record in online_records:
         csvwriter.writerow(online_record)
